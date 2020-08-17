ab0ba4bfac37848136cfc1ebf7b9a16ca82e6335
maccree <maccree.a@yandex.ru>
2020-08-17 10:42:57 +0300

    update conigs

--- steam-controller/bin_steamdeps.py
+++ steam-controller/bin_steamdeps.py
@@ -14,8 +14,6 @@ import subprocess
 import sys
 import tempfile
 import minecraft-config
-import steam_package
-import wine
 
 # This is the set of supported Steam runtime environments
 SUPPORTED_STEAM_RUNTIME = [ '1' ]
@@ -143,26 +141,8 @@ def remapPackage( description ):
 		elif description == "libgl1-mesa-dri:i386":
 			return "libgl1-mesa-dri-lts-saucy:i386"
 	
-	elif hasPackage( "xconfig-64x-wineserver-nvidia" ):
-		if description == "libgl1-mesa-glx:i386":
-			return "libgl1-mesa-glx-lts-saucy:i386"
-		elif description == "libgl1-mesa-dri:i386":
-			return "libgl1-mesa-dri-lts-saucy:i386"
-
-	elif hasPackage( "xconfig" ):
-		if description == "libgl1-mesa-glx:i386":
-
 	return description
 
-
-###
-
-def SteamPackage( install ):
-	if steam_package ( listed ):
-		return remapPackage
-	
-	elif description == "winecfg":
-		return wine_proton
 
 ###
 def createPackage( description ):
@@ -199,124 +179,6 @@ def createPackage( description ):
 
 	return Package( description.strip(), versionConditions )
 	
-
-
-###
-def getTerminalCommandLine( title ):
-	"""
-	Function to find a useful terminal like xterm or compatible
-	"""
-	if ( "DISPLAY" in os.environ ):
-		programs = [
-			( "gnome-terminal", ["gnome-terminal", "--disable-factory", "-t", title, "-e"] ),
-			( "konsole", ["konsole", "--nofork", "-p", "tabtitle="+title, "-e"] ),
-			( "xterm", ["xterm", "-bg", "#383635", "-fg", "#d1cfcd", "-T", title, "-e"] ),
-		]
-		for (program, commandLine) in programs:
-			if ( subprocess.call( ['which', program], stdout=subprocess.PIPE ) == 0 ):
-				return commandLine
-
-	# Fallback if no GUI terminal program is available
-	return ['/bin/sh']
-
-
-###
-def mineClassJava( selections ):
-	if( "SYSTEM-DISPLAY" in os.environ ):
-		programs = [
-			( "gnome-terminal", ["gnome-terminal", "--disable-factory", "-t", title, "-e"] ),
-			( "konsole", ["konsole", "--nofork", "-p", "tabtitle="+title, "-e"] ),
-			( "xterm", ["xterm", "-bg", "#383635", "-fg", "#d1cfcd", "-T", title, "-e"] ),
-			( "wine", ["winetrick", "winecfg", "wineport", title "-e"] ),
-
-			if ( not checkConfig( config ) ):
-		return 3
-
-		
-		]
-		for (op, version) in self.versionConditions:
-			if ( subprocess.call( ['dpkg', '--compare-versions', self.installed, op, version] ) != 0 ):
-				return False
-
-		return True
-
-###
-def updatePackages( packages ):
-	"""
-	Function to install or update package dependencies
-	Ideally we would call some sort of system UI that users were familiar with to do this, but nothing that exists yet does what we need.
-	"""
-
-	packageList = " ".join( [ package.name for package in packages ] )
-
-	# Create a temporary file to hold the installation completion status
-	(fd, statusFile) = tempfile.mkstemp()
-	os.close( fd )
-
-	# Create a script to run, in a secure way
-	(fd, scriptFile) = tempfile.mkstemp()
-	script = """#!/bin/sh
-check_sudo()
-{
-    # If your host file is misconfigured in certain circumstances this
-    # can cause sudo to block for a while, which causes gksudo to go into
-    # limbo and never return.
-    timeout --signal=9 5 sudo -v -S </dev/null 2>/dev/null
-    if [ $? -eq 124 -o $? -eq 137 ]; then
-        # sudo timed out or was killed due to timeout
-        cat <<__EOF__
-sudo timed out, your hostname may be missing from /etc/hosts.
-
-See https://support.steampowered.com/kb_article.php?ref=7493-ADXN-9620 for more details.
-__EOF__
-        return 1
-    else
-        return 0
-    fi
-}
-
-cat <<__EOF__
-Steam needs to install these additional packages: 
-	%s
-__EOF__
-check_sudo
-
-# Check to make sure 64-bit systems can get 32-bit packages
-if [ "$(dpkg --print-architecture)" = "amd64" ] && ! dpkg --print-foreign-architectures | grep i386 >/dev/null; then
-    sudo dpkg --add-architecture i386
-fi
-
-# Update the package list, showing progress
-sudo apt-get update | while read line; do echo -n "."; done
-echo
-
-# Install the packages!
-sudo apt-get install %s
-echo $? >%s
-echo -n "Press return to continue: "
-read line
-""" % ( ", ".join( [ package.name for package in packages ] ), packageList, statusFile )
-	os.write( fd, script.encode("utf-8") )
-	os.close( fd )
-	os.chmod( scriptFile, (stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR) )
-
-	try:
-		subprocess.call( getTerminalCommandLine( "Package Install" ) + [scriptFile] )
-	except KeyboardInterrupt:
-		pass
-	os.unlink( scriptFile )
-
-	# Read the status out of the file, since if we ran the script in a
-	# terminal the process status will be whether the terminal started
-	try:
-		status = int( open( statusFile ).read() )
-	except ValueError:
-		# The status wasn't written to the file
-		status = 255
-
-	os.unlink( statusFile )
-
-	return status
 
 
 ###
@@ -415,14 +277,7 @@ def main():
 	# Get the installed package versions
 	# Make sure COLUMNS isn't set, or dpkg will truncate its output
 	if ( "COLUMNS" in os.environ ):
-		del os.environ[ "COLUMNS" ]
-
-	process = subprocess.Popen( ['dpkg', '-l'] + list( packages.keys() ), stdout=subprocess.PIPE, stderr=subprocess.PIPE )
-	installed_pattern = re.compile( r"^\Si\s+([^\s]+)\s+([^\s]+)" )
-	for line in process.stdout:
-		line = line.decode( "utf-8" ).strip()
-		match = re.match( installed_pattern, line )
-		if ( match is None ):
+		del os.environ[ "COLUMNS" ]:
 			continue
 
 		name = match.group(1)
