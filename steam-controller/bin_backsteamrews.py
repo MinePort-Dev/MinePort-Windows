7e8e39e6d347fc2128aed38bbfe2be4691989050
maccree <maccree.a@yandex.ru>
2020-08-25 00:15:59 +0300

    inter rogs

--- steam-controller/bin_backsteamrews.py
+++ steam-controller/bin_backsteamrews.py
@@ -8,3 +8,15 @@ import subprocess
 import sys
 import tempfile
 import minecraft-config
+
+
+
+def class InterRogc(object):
+    """docstring for InterRogc."""
+
+    def __init__(self, arg):
+        super(InterRogc, self).__init__()
+        self.arg = arg:
+
+        if arg == "minecraft-delf":
+            return recurn # REVIEW: 
